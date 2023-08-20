a801e7dd17fa8a15928e6f3745ff9dbffb185e6f
Jiayii03 <116914556+Jiayii03@users.noreply.github.com>
2023-08-20 18:05:11 +0800

    Learning new project Promptopia not completed

---
+++ promptopia/app/page.jsx
@@ -0,0 +1,22 @@
+import Feed from '@components/Feed'
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" /> 
+        {/* used to conditionally hide a line break element hwen a specified width breakpoint is reached  */}
+        {/* <br /> */}
+        <span className="orange_gradient text-centre"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source project that uses GPT-3 to generate writing prompts.
+      </p>
+
+      <Feed />
+    </section>
+  )
+}
+
+export default Home
